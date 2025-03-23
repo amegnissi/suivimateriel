476af113618cb52f489112c7f2d1506c10409f55
dodji05 <gildas31@gmail.com>
2025-03-23 02:51:57 +0100

    installation et configuration du template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,7 +11,7 @@ final class IndexController extends AbstractController
     #[Route('/index', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
+        return $this->render('baseC.html.twig', [
             'controller_name' => 'IndexController',
         ]);
     }
